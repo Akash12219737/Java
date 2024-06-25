0734f215548d663b4e8f127309dc24c1f672df33
Akash Maurya <119936164+Akash12219737@users.noreply.github.com>
2024-06-25 21:23:57 +0530

    Add files via upload

---
+++ OOPs/Person.java
@@ -0,0 +1,25 @@
+// File name: Person.java
+
+public class Person {
+    private String name;
+    private int age;
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method to display person details
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        // Creating an object of Person class
+        Person person = new Person("John", 25);
+        // Displaying person details
+        person.display();
+    }
+}
