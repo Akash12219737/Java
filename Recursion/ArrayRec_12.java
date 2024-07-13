43ac07eb490db7e316287bcb5200c2d745fb60f2
Akash Maurya <119936164+Akash12219737@users.noreply.github.com>
2024-07-13 09:31:51 +0530

    Add files via upload

---
+++ Recursion/ArrayRec_12.java
@@ -0,0 +1,17 @@
+public class ArrayRec_12 {
+    static void printArray(int[] arr, int idx){
+        if (idx == arr.length) {
+            return;
+            
+        }
+        // self work
+        System.out.println(arr[idx]);
+
+        // recursion
+        printArray(arr, idx+1);
+    }
+    public static void main(String[] args) {
+        int [] arr = {5, 6, 7, 8,9};
+        printArray(arr, 0);
+    }
+}
